0c205eb3dfaeac41e31ce77d18c7750d15910dd7
clearsky77 <clearsky3264@gmail.com>
2021-09-26 12:31:28 +0900

    - 토스트 띄우기 에러 해결. requireContext()활용.

--- app/src/main/java/com/clearsky77/fragment_20210926/fragments/SecondFragment.kt
+++ app/src/main/java/com/clearsky77/fragment_20210926/fragments/SecondFragment.kt
@@ -1,25 +1,31 @@
 package com.clearsky77.fragment_20210926.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.clearsky77.fragment_20210926.R
+import kotlinx.android.synthetic.main.fragment_second.*
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        Log.d("첫 부품", "버튼 눌림")
+
+        toastBtn.setOnClickListener {
+//            토스트를 불러내는 화면은 this로 하면 fragment라서 에러가 난다.
+            Toast.makeText(requireContext(), "프래그먼트 토스트", Toast.LENGTH_SHORT).show()
+        }
     }
+
 
 }
