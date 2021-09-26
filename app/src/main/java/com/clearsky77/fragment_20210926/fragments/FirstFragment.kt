48db3af37921283d430e48f64a721b20c2f3f2a4
clearsky77 <clearsky3264@gmail.com>
2021-09-26 12:04:13 +0900

    Fragment의 동작 코드 - onActivityCreated 활용

---
+++ app/src/main/java/com/clearsky77/fragment_20210926/fragments/FirstFragment.kt
@@ -0,0 +1,25 @@
+package com.clearsky77.fragment_20210926.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.clearsky77.fragment_20210926.R
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.d("첫 부품", "버튼 눌림")
+    }
+
+}
